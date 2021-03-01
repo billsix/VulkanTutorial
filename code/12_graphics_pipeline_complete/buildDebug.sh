1f3085ac917152f34d920d4dce99e4def5af0603
William Emerison Six <billsix@gmail.com>
2021-02-28 22:02:59 -0500

    Cmake-ify the codebase
    Gave each project it's own CMake file

---
+++ code/12_graphics_pipeline_complete/buildDebug.sh
@@ -0,0 +1,10 @@
+mkdir build
+mkdir buildInstall
+cd build
+
+cmake -DCMAKE_INSTALL_PREFIX=../buildInstall -DCMAKE_BUILD_TYPE=Debug ../
+cmake --build  . --target all
+cmake --build  . --target install
+cd ../buildInstall
+./bin/12_graphics_pipeline_complete
+cd ..
---
+++ code/21_descriptor_layout/buildDebug.sh
@@ -0,0 +1,10 @@
+mkdir build
+mkdir buildInstall
+cd build
+
+cmake -DCMAKE_INSTALL_PREFIX=../buildInstall -DCMAKE_BUILD_TYPE=Debug ../
+cmake --build  . --target all
+cmake --build  . --target install
+cd ../buildInstall
+./bin/21_descriptor_layout
+cd ..
